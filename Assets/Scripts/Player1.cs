5ecbe27914fab239aacc1bde9795538af7e4ced6
DesireeNavarrete <desiree.nav96@gmail.com>
2024-01-27 04:29:46 +0100

    moviemiento real babyyyyyyy hasta la muelteeeee

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -7,23 +7,39 @@ public class Player1 : MonoBehaviour
 {
     public float rotationSpeed;
     public float moveHorizontal;
+    public float speed, speeddecrease, maxspeed;
+    public float plusspeed = 2;
+
     void Start()
     {
 
     }
-
-    
-
-
     private void Update()
     {
 
         moveHorizontal = Input.GetAxis("P1");
 
-        Vector3 movement = new Vector3(0, moveHorizontal*15, 0);
+        Vector3 movement = new Vector3(0, moveHorizontal * 15, 0);
         transform.Rotate(movement * rotationSpeed * Time.deltaTime);
 
-        transform.Translate(Vector3.forward * Time.deltaTime);
+        print(Input.GetButtonDown("reee"));
+        if (Input.GetButtonDown("reee"))
+        {
+
+            speed += plusspeed;
+        }
+        speed -= speeddecrease;
+        if (speed < 0)
+        {
+            speed = 0;
+        }
+        if (speed > maxspeed)
+        {
+            speed = maxspeed;
+
+        }
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
 }
--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using XInputDotNetPure;
 
 public class Player2 : MonoBehaviour
 {
     public float moveHorizontal;
     public float rotationSpeed;
+    public float speed, speeddecrease, maxspeed;
+    public float plusspeed = 2;
+
     void Start()
     {
 
     }
-
-    
 
     private void Update()
     {
@@ -22,7 +24,23 @@ public class Player2 : MonoBehaviour
         Vector3 movement = new Vector3(0, moveHorizontal*15, 0);
         transform.Rotate(movement * rotationSpeed * Time.deltaTime);
 
-        transform.Translate(Vector3.forward * Time.deltaTime);
+        print(Input.GetButtonDown("c1rrr"));
+        if (Input.GetButtonDown("c1rrr"))
+        {
+            speed += plusspeed;
+        }
+        speed -= speeddecrease;
+        if (speed < 0)
+        {
+            speed = 0;
+        }
+        if (speed > maxspeed)
+        {
+            speed = maxspeed;
+
+        }
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
 }
